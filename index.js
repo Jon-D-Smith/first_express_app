2c99419d25c0c08058614892b318e65bf4643068
Jon Smith <aardvarkjon94@yahoo.com>
2021-03-29 12:33:10 -0500

    Setup initial route; Sent initial data

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(() => {
-    console.log("We got a new request");
+// app.use((req, res) => {
+//     console.log("We got a new request");
+//     res.send("Hello app.use")
+// })
+
+app.get('/', (req, res) => {
+    res.send("Hello World!")
 })
 
 
